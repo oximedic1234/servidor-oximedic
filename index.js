7a2e3942b52d4aaea737a6d99d3beef55f662620
abraham199803 <abraham1234567891014@gmail.com>
2026-05-18 23:26:20 -0400

    conexion proxy ipv4 nativo

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 // =======================================================
 const pool = new Pool({
   // REEMPLAZA [YOUR-PASSWORD] CON TU CONTRASEÑA DE SUPABASE (SIN LOS CORCHETES)
-connectionString: "postgresql://postgres:oximedic2026@db.ixuhvkxtzlraaznyjwdt.supabase.co:5432/postgres",
+connectionString: "postgresql://postgres:oximedic2026@aws-0-sa-east-1.pooler.supabase.com:5432/postgres?sslmode=require",
   ssl: { rejectUnauthorized: false } 
 });
 
